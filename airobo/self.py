deb569483e03df2e21c80da5170fb1f7b9b8022d
jedi24garcia <jedi24garcia@gmail.com>
2022-10-16 19:07:07 +1300

    added microphone

--- airobo/self.py
+++ airobo/self.py
@@ -6,12 +6,13 @@ import wikipedia
 import wolframalpha
 #import comtypes
 
-print("Welcome fellow humans.")
+print("Welcome fellow humans.") #sample response
 
 r = sr.Recognizer()
+r = sr.Microphone()
 
 class SelfSpeak():
   def __init__(self, speak):
     self = speak
 
-time.sleep(3)
+'''time.sleeps(3)'''
