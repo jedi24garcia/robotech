df1798951e6ff823b07716b5e9b76635765014c1
jedi24garcia <jedi24garcia@gmail.com>
2022-10-19 11:16:47 +1300

    deleted irrelevant lines

--- airobo/self.py
+++ airobo/self.py
@@ -8,33 +8,18 @@ import datetime
 
 print("Welcome humans.")
 
-User_Input = input("Please enter your message here: ")
+self_response  = input("Please enter your message here: ")
 
-selfResponse = ("How are you?")
+UserInput = ("How are you?")
 
 def desire():
-  if User_Input == selfResponse:
+  if self_response == UserInput:
     print("Im good, how about yourself? ")
   else:
-    print("Type again")
+    print("I'm sorry, please try again.")
 
 desire()
 
-# This function obtain audio from microphone
-"""
-def selfCommand():
-  r = sr.Recognizer()
-  with sr.Microphone as source:
-    print("Listening.. ")
-    audio = r.listen(source, offset=4, duration=5)
-  try:
-    statement=r.recognize_google(audio,language='en-in')
-    print("Google: you said\n")
-  # print("Google: you said\n" + sr.recognizer_google(audio))
-  except Exception as e:
-    print("Forgive me, please try again")
-    return "None"
-"""
 
 
-# will need to make microphone work and computer say "print("Welcome fellow humans.")"
+# will need to make computer response using speech
