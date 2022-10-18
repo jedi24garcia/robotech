3e072f681ec3fbd596bc0c233d9cc8dcc0e129aa
jedi24garcia <jedi24garcia@gmail.com>
2022-10-19 11:21:50 +1300

    added another statement

--- airobo/self.py
+++ airobo/self.py
@@ -11,10 +11,14 @@ print("Welcome humans.")
 self_response  = input("Please enter your message here: ")
 
 UserInput = ("How are you?")
+UserInput2 = ("Hi")
+
 
 def desire():
   if self_response == UserInput:
     print("Im good, how about yourself? ")
+  if self_response == UserInput2:
+    print("Hello")
   else:
     print("I'm sorry, please try again.")
 
