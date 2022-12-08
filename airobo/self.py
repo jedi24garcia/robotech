42d863dc6d2c98d797f10933b5f1a62e314c9fa3
jedi24garcia <jedi24garcia@gmail.com>
2022-12-08 21:51:53 +1300

    removed comments

--- airobo/self.py
+++ airobo/self.py
@@ -29,13 +29,13 @@ def speak(text):
 def wishMe():
   hour=datetime.datetime.now().hour
   if hour>=0 and hour<12:
-  # speak("Hello, Good Morning")
+    speak("Hello, Good Morning")
     print("Hello, Good Morning")
   elif hour>=12 and hour<12:
-  # speak("Hello, Good Afternoon")
+    speak("Hello, Good Afternoon")
     print("Hello, Good Afternoon")
   else:
-  # speak("Hello, Good Evening")
+    speak("Hello, Good Evening")
     print("Hello, Good Evening")
 
 def speakCommand():
