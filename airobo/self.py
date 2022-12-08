4cd79051df9319d0ba88501a0ba85bddcdaf4a0b
jedi24garcia <jedi24garcia@gmail.com>
2022-12-08 22:02:44 +1300

    start from scratch

--- airobo/self.py
+++ airobo/self.py
@@ -3,7 +3,6 @@
 import speech_recognition as sr
 import pyttsx3
 import wikipedia
-# import wolframalpha
 import datetime
 
 print("Welcome!")
@@ -11,46 +10,10 @@ print("Welcome!")
 input_user = input("Say something: ")
 
 engine = pyttsx3.init()
-# GivenSpeech = "I’m increasingly inclined to think that there should be some regulatory oversight, maybe at the national and international level, just to make sure that we don’t do something very foolish. I mean with artificial intelligence we’re summoning the demon"
-
-# will need to make input_user speak when user run program 
-'''
-def UserInputSpeak():
-  engine.say()
-  engine.runAndWait()
-'''
-
-engine.setProperty('voice', 'voices[0].id')
 
 def speak(text):
   engine.say(text)
   engine.runAndWait()
 
-def wishMe():
-  hour=datetime.datetime.now().hour
-  if hour>=0 and hour<12:
-    speak("Hello, Good Morning")
-    print("Hello, Good Morning")
-  elif hour>=12 and hour<12:
-    speak("Hello, Good Afternoon")
-    print("Hello, Good Afternoon")
-  else:
-    speak("Hello, Good Evening")
-    print("Hello, Good Evening")
-
-def speakCommand():
-   r = sr.Recognizer()
-   with sr.Microphone() as source:
-    print("Listening...")
-    audio=r.listen(source)
-
-rate = engine.getProperty("rate")
-print(rate)
-engine.setProperty("rate", 180)
-
-# speak(GivenSpeech) # say these words slower
-# UserInputSpeak(input_user)
 speak("Hello, user!")
-wishMe()
 
-#speak(input_user) - Did not work
