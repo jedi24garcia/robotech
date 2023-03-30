484130b50fe27f6a0f4769d1a1a758ab7c02e751
jedi24garcia <jedi24garcia@gmail.com>
2023-03-30 21:34:26 +1300

    added image to frame

--- airobo/frame.py
+++ airobo/frame.py
@@ -2,11 +2,15 @@
 
 import tkinter as tk
 from tkinter import *
+# from PIL import ImageTK, Image
+from PIL import Image, ImageTk
 
 window = tk.Tk()
 
 window.title("Kobe")
 window.geometry("500x400")
 window.configure(bg="black")
+
+img = ImageTk.PhotoImage(Image.open("cool_city.jpg"))
 
 window.mainloop()
