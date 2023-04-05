d6ec043703d1321279825c245f53adb090a6543d
jedi24garcia <jedi24garcia@gmail.com>
2023-04-05 13:35:30 +1200

    tkinter gif

--- airobo/frame.py
+++ airobo/frame.py
@@ -10,7 +10,8 @@ window.title("Future City")
 window.geometry("800x900+100+100")
 window.configure(bg="black")
 
-img = ImageTk.PhotoImage(Image.open("kobe_bryant.jpg"))
+# img = ImageTk.PhotoImage(Image.open("kobe_bryant.jpg"))
+img = ImageTk.PhotoImage(Image.open("kobe_24.gif"))
 
 label = Label(window, image = img)
 label.pack()
